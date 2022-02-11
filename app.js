5b2299d8e5908d5e703d1683544bdc722126c778
Lars-Magnus Skog <ralphtheninja@riseup.net>
2022-02-11 14:59:42 +0100

    Move calculation of split weeks to use

--- app.js
+++ app.js
@@ -25,29 +25,26 @@ const {
 app.use((state, emitter) => {
   // TODO set current month
   state.current = { year: 2022, month: 2 }
-  state.current.monthDaysFilled = monthDaysFilled(state.current)
+  state.current.monthWeeks = daysToWeeks(monthDaysFilled(state.current))
 
   emitter.on('month:prev', () => {
-    // TODO recalculate weeks
     const prev = previousMonth(state.current)
     state.current.month = prev.month
     state.current.year = prev.year
-    state.current.monthDaysFilled = monthDaysFilled(state.current)
+    state.current.monthWeeks = daysToWeeks(monthDaysFilled(state.current))
     emitter.emit('render')
   })
   emitter.on('month:next', () => {
-    // TODO recalculate weeks
     const next = nextMonth(state.current)
     state.current.month = next.month
     state.current.year = next.year
-    state.current.monthDaysFilled = monthDaysFilled(state.current)
+    state.current.monthWeeks = daysToWeeks(monthDaysFilled(state.current))
     emitter.emit('render')
   })
 })
 
 app.route('*', (state, emit) => {
-  const { year, month, monthDaysFilled } = state.current
-  const weeks = daysToWeeks(monthDaysFilled)
+  const { year, month, monthWeeks } = state.current
 
   return html`<body class=${body}>
     <div class=${calendar}>
@@ -57,7 +54,7 @@ app.route('*', (state, emit) => {
         <div class=${calendarHeaderButton} onclick=${gotoNextMonth}>${'>'}</div>
       </div>
       <div class=${weekdaysHeader}>${WEEKDAYS.map(renderWeekdayCell)}</div>
-      <div class=${monthContainer}>${weeks.map(renderWeek)}</div>
+      <div class=${monthContainer}>${monthWeeks.map(renderWeek)}</div>
     </div>
   </body>`
 
