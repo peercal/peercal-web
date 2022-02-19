63cf6b5fd0c0245505793bc603e043bc1fe5a71f
Lars-Magnus Skog <ralphtheninja@riseup.net>
2022-02-20 00:46:08 +0100

    Add linkping data

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ app.use((state, emitter) => {
   // TODO configure colors when adding feeds from the ui, with sane defaults
   addReadableFeed('hyper://0aa6537ae8f41113c583d725305944f00281968b0d23051804361a3436ea4e38/events.ics', 'white', 'black')
   addReadableFeed('hyper://3fe48c75e45aae82ef90cf29027f78fa821eb35c247e7e80dae6dba5105f5909/events.ics', 'blue', 'white')
+  addReadableFeed('hyper://7805b255174ccf9e6e8af13ea71bcb9ad347a817984e94e189b1d45eb9fa88d1/events.ics', 'orange', 'black')
 
   function setMonthly (monthly) {
     state.monthly = monthly
