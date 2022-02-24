5ea84c7d144aeacadce44749bed4fba12206cab0
Lars-Magnus Skog <ralphtheninja@riseup.net>
2022-02-24 19:52:08 +0100

    Add a link to the code as well

--- components/footer.js
+++ components/footer.js
@@ -7,11 +7,14 @@ const footer = css`
     bottom: 0px;
     left: 20px;
     right: 20px;
+    display: flex;
+    justify-content: space-between;
   }
 `
 
 module.exports = () => {
   return html`<div class=${footer}>
      <div>Made with <a href='https://www.choo.io/' target='new'>🚂</a></div>
+     <div><a href='https://github.com/peercal/peercal' target='new'><i>Code</i></a></div>
   </div>`
 }
