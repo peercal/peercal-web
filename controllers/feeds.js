b7a2208cc2b1c45323feb87e93dc0973ab2f941f
Lars-Magnus Skog <ralphtheninja@riseup.net>
2022-02-28 17:47:55 +0100

    Add url and readOnly to aggregated events

--- controllers/feeds.js
+++ controllers/feeds.js
@@ -11,16 +11,16 @@ module.exports = ({ feeds, swarmOpts }) => {
     function addReadOnlyFeed ({ url, background, color }) {
       console.log('Adding read only feed', url)
       const watcher = HyperdriveWatcher({ url, onFeedUpdate, swarmOpts })
-      const feed = { events: [], watcher, background, color }
+      const feed = { events: [], watcher, background, color, url, readOnly: true }
       state.feeds.set(url, feed)
     }
 
     function aggregateAllEvents () {
       let result = []
       for (const feed of state.feeds.values()) {
-        const { background, color } = feed
+        const { background, color, url, readOnly } = feed
         const events = feed.events.map(ev => {
-          return { ...ev, background, color }
+          return { ...ev, background, color, url, readOnly }
         })
         result = result.concat(events)
       }
