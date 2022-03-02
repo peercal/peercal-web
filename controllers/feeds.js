a2a701b1b4989eb9907c7bf3433a257a43d4f0b8
Lars-Magnus Skog <ralphtheninja@riseup.net>
2022-03-02 13:03:04 +0100

    Pass in opts to VEvent

--- controllers/feeds.js
+++ controllers/feeds.js
@@ -11,25 +11,22 @@ module.exports = ({ feeds, swarmOpts }) => {
     function addReadOnlyFeed ({ url, background, color }) {
       console.log('Adding read only feed', url)
       const watcher = HyperdriveWatcher({ url, onFeedUpdate, swarmOpts })
-      const feed = { events: [], watcher, background, color, url, readOnly: true }
+      const feed = { events: [], watcher, background, color, readOnly: true }
       state.feeds.set(url, feed)
     }
 
     function aggregateAllEvents () {
       let result = []
       for (const feed of state.feeds.values()) {
-        const { background, color, url, readOnly } = feed
-        const events = feed.events.map(ev => {
-          return { ...ev, background, color, url, readOnly }
-        })
-        result = result.concat(events)
+        result = result.concat(feed.events)
       }
       return result
     }
 
     function onFeedUpdate ({ url, data }) {
       const feed = state.feeds.get(url)
-      feed.events = parseEvents(data)
+      const { background, color, readOnly } = feed
+      feed.events = parseEvents(data, { url, background, color, readOnly })
       state.allEvents = aggregateAllEvents()
       emitter.emit('feeds:update')
     }
