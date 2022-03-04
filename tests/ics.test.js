1a309dbbf597898dfd0f01acdb9c5894cf7056af
Lars-Magnus Skog <ralphtheninja@riseup.net>
2022-03-04 15:49:10 +0100

    Change tests to read a file with \r\n endings
    The lines in the icalendar format ends with \r\n and
    the tests should consider this instead of testing on
    a javascript template literate

--- tests/ics.test.js
+++ tests/ics.test.js
@@ -1,9 +1,12 @@
 const test = require('ava')
+const fs = require('fs')
 const {
   VEvent,
   parseEvents,
   filterEventsFromDate
 } = require('../lib/ics.js')
+
+const DATA = fs.readFileSync('./tests/events.ics', 'utf-8')
 
 test('VEvent class, empty constructor', t => {
   const event = new VEvent()
@@ -61,91 +64,3 @@ test('filtering events', t => {
   t.is(filterEventsFromDate(events, new Date('2022-03-02')).length, 0)
   t.is(filterEventsFromDate(events, new Date('2022-03-03')).length, 0)
 })
-
-const DATA = `
-BEGIN:VCALENDAR
-PRODID:-//Foobar GmbH
-VERSION:2.0
-CALSCALE:GREGORIAN
-METHOD:PUBLISH
-
-begin:vevent
-DTSTART:20211013T050000Z
-DTEND:20211013T080000Z
-DTSTAMP:20220205T165901Z
-UID:MlN8btK----21633566522596@example.com
-SEQUENCE:1
-SUMMARY:Watch amazing movie "Kong vs Godzilla"
-DESCRIPTION:The description field should be a longer text describing the event in more details.
-ORGANIZER;EMAIL=linkping@example.com:mailto:linkping@example.com
-end:vevent
-
-BEGIN:VEVENT
-DTSTART;VALUE=DATE:20220301
-DTEND;VALUE=DATE:20220302
-DTSTAMP:20220301T132320Z
-UID:MlN8btK----21646140741358@tutanota.com
-SEQUENCE:1
-SUMMARY:Fettisdagen
-DESCRIPTION:<div dir="auto">Eat a semla! <br></div>
-LOCATION:All over the world
-ORGANIZER;EMAIL=linkping@tutanota.com:mailto:linkping@tutanota.com
-END:VEVENT
-
-BEGIN:VEVENT
-DTSTART:20211025T040000Z
-DTEND:20211025T043000Z
-DTSTAMP:20220205T165901Z
-UID:MlN8btK----21633981081958@example.com
-SEQUENCE:1
-SUMMARY:3D-printing
-DESCRIPTION:<div dir="auto">Obviously we can put html in here as well. The question is if we should render it or not.</div>
-ORGANIZER;EMAIL=linkping@example.com:mailto:linkping@example.com
-END:VEVENT
-
-BEGIN:VEVENT
-DTSTART:20220108T150000Z
-DTEND:20220108T180000Z
-DTSTAMP:20220205T165901Z
-UID:MlN8btK----21641477558250@example.com
-SEQUENCE:2
-SUMMARY:3D-modelling workshop
-DESCRIPTION:<div dir="auto">At the last workshop we installed freecad, so if you don't have it installed it can be a good idea to install it before the workshop.<br></div><div dir="auto">Ending time is approximate.&nbsp</div><div dir="auto"><a href="https://www.freecadweb.org/" rel="noopener noreferrer" target="_blank">https://www.freecadweb.org/</a><br></div><div dir="auto"><br></div>
-ORGANIZER;EMAIL=linkping@example.com:mailto:linkping@example.com
-END:VEVENT
-
-BEGIN:VEVENT
-DTSTART:20220129T170000Z
-DTEND:20220129T200000Z
-DTSTAMP:20220205T165901Z
-UID:MlN8btK----21642798024486@example.com
-SEQUENCE:3
-SUMMARY:3D-printing workshop
-DESCRIPTION:<div dir="auto">Were going to try to 3D-print or at least model an idea that Godogg has for his car.&nbsp<br></div><div dir="auto">We're using freecad - its fine to come if you haven't been to the other workshops, but you might want to install freecad ahead of time.&nbsp<br></div><div dir="auto">Welcome!&nbsp<br></div><div dir="auto"><br></div>
-ORGANIZER;EMAIL=linkping@example.com:mailto:linkping@example.com
-END:VEVENT
-
-BEGIN:VEVENT
-DTSTART:20220129T200000Z
-DTEND:20220129T210000Z
-DTSTAMP:20220205T165901Z
-UID:MlN8btK----21642798024486@example.com
-SEQUENCE:3
-SUMMARY:3D-printing workshop (cont)
-DESCRIPTION:<div dir="auto">Were going to try to 3D-print or at least model an idea that Godogg has for his car.&nbsp<br></div><div dir="auto">We're using freecad - its fine to come if you haven't been to the other workshops, but you might want to install freecad ahead of time.&nbsp<br></div><div dir="auto">Welcome!&nbsp<br></div><div dir="auto"><br></div>
-ORGANIZER;EMAIL=linkping@example.com:mailto:linkping@example.com
-END:VEVENT
-
-BEGIN:VEVENT
-DTSTART:20220219T120000Z
-DTEND:20220226T120000Z
-DTSTAMP:20220205T165901Z
-UID:MlN8btK----21633566522596@example.com
-SEQUENCE:1
-SUMMARY:Watch amazing movie "Kong vs Godzilla"
-DESCRIPTION:The description field should be a longer text describing the event in more details.
-ORGANIZER;EMAIL=linkping@example.com:mailto:linkping@example.com
-END:VEVENT
-
-END:VCALENDAR
-`
